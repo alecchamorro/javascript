374bed58a9a80d409d62293d63cb12245bf54528
alecchamorro <alecchamorro@gmail.com>
2021-10-21 14:54:29 -0300

    commit 1

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var $edad="20";
+var $nombre="juan";
+var $apellido="carlos";
+
+// console.log($edad)
+
+let $dni="40.000.000";
+let $altura="182";
+let $direccion="calle falsa 123"
+
+// console.log($dni)
+
+const $colorPreferido="azul";
+const $club="boca";
+const $espiritu="como estas";
+
+// alert($colorPreferido)
+// console.log($edad+$altura);
+
+// console.log($mascota);
+
+alert("Hola" +" "+$nombre+" "+ $apellido+" "+ $espiritu);
+let $nombreyUsuarui=prompt("porque aca esta el espiritu")
+console.log($edad*$altura)
