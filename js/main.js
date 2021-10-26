fbff9259ca5367bcbcf15af1e1fbd7346098a3d8
alecchamorro <alecchamorro@gmail.com>
2021-10-26 15:12:25 -0300

    update clase2

--- js/main.js
+++ js/main.js
@@ -1,28 +1,53 @@
-// Clase 1
+// ACTIVIDAD CLASE 2
 
-// var $edad="20";
-// var $nombre="juan";
-// var $apellido="carlos";
+const $edad= Number(prompt("ingrese tu edad"));
+const $user= prompt("ingrese Hola");
+const $number= Number(prompt("ingrese un numero mayor a 1000"));
 
-// // console.log($edad)
 
-// let $dni="40.000.000";
-// let $altura="182";
-// let $direccion="calle falsa 123"
+if($user=="Hola") {
+    console.log("bienvenido " + $user);
+}
 
-// // console.log($dni)
+else{
+    console.log("bienvenido usuario invitado")
+}
 
-// const $colorPreferido="azul";
-// const $club="boca";
-// const $espiritu="como estas";
+if($edad<10) {
+    console.log("no podes ingresar, sos menor")}
 
-// // alert($colorPreferido)
-// // console.log($edad+$altura);
+else if($edad>50) {
+    console.log("acceso permitido")
+}
 
-// // console.log($mascota);
+else {
+    console.log("entre 10 y 50")
+}
 
-// alert("Hola" +" "+$nombre+" "+ $apellido+" "+ $espiritu);
-// let $nombreyUsuarui=prompt("porque aca esta el espiritu")
-// console.log($edad*$altura)
+if($number>1000){
+    console.log("numero mayor a 1000")
+}
 
-// Clase 2
+console.log("Hola chicxs")
+
+
+// admin: admin:admin123
+// user1: carlos:carlos123
+// user2: david:David123
+// user3: ale:
+
+
+// const $usuario_valido = ($user=="carlos" && $pass=="carlos123") || ($user=="david" && $pass=="david123") || ($user=="ale" && $pass=="ale123")
+// const $admin=($user=="admin" && $pass=="admin123")
+
+// const $user= prompt("ingrese usuario");
+// const $pass= prompt("ingrese clave");
+
+
+// if ($admin) {
+//     console.log("bienvenido dios");
+//  } else if ($usuario_valido) {
+//         console.log("bienvenido "+$user);
+// } else{
+//     console.log("datos incorrectos");
+//     }
