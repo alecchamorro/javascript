5ce2ddc6e2bec7ee99befcc9e558d9cdddb433ac
alecchamorro <alecchamorro@gmail.com>
2022-06-07 12:15:59 -0300

    simulador interactivo

--- js/main.js
+++ js/main.js
@@ -1,32 +1,55 @@
-let ingreso = prompt(
-    "Escoja un genero \n rock \n pop \n cumbia \n reggaeton \n electro\n\n Para finalizar ingrese SALIR"
-  );
-  if (ingreso == "SALIR") alert("Genial, buenos gustos! saludos");
-  
-  while (ingreso != "SALIR") {
-    switch (ingreso) {
-      case "rock":
-        alert("excelente gusto!");
-        break;
-      case "pop":
-        alert("bien, buena musica");
-        break;
-      case "cumbia":
-        alert("algo movida va bien!");
-        break;
-      case "reggaeton":
-        alert("epa, es la que va");
-        break;
-      case "electro":
-        alert("excelente");
-        break;
-  
-      default:
-        alert("mm no conozco ese genero");
-        break;
+
+function saludar() {
+ 	alert("Calculadora");
+ }
+
+saludar();
+
+ function sumar(x = 0, y = 0) {
+ 	return x + y;
+ 	console.log("Esto no se ejecuta");
     }
-    ingreso = prompt(
-      "Escoja un genero \n rock \n pop \n cumbia \n reggaeton \n electro\n\n Para finalizar ingrese SALIR"
-    );
-    if (ingreso == "SALIR") alert("Genial, buenos gustos! saludos");
-  }
+
+ function restar(x, y) {
+ 	const resultado = x - y;
+ 	alert(`El resultado de ${x}-${y} es ${resultado}`);
+ 	return resultado;
+ }
+ function multiplicar(x, y) {
+ 	const resultado = x * y;
+ 	alert(`El resultado de ${x}*${y} es ${resultado}`);
+ }
+ function dividir(x, y) {
+ 	const resultado = x / y;
+ 	alert(`El resultado de ${x}/${y} es ${resultado}`);
+ }
+
+ const operacion = Number(prompt(`Bienvenideo, que operacion queres realizar?
+ 1 - Sumar
+ 2 - Restar
+ 3 - Multiplicar
+ 4 - Dividir`));
+
+ const numero1 = Number(prompt("Ingresa el primer número"));
+ const numero2 = Number(prompt("Ingresa el segundo número"));
+
+ switch (operacion) {
+ 	case 1:
+ 		const suma = sumar(numero1, numero2);
+ 		alert(`El resultado de ${numero1}+${numero2} es ${suma}`);
+ 		break;
+ 	case 2:
+ 		const resta = restar(numero1, numero2);
+ 		console.log(resta);
+ 		break;
+ 	case 3:
+ 		multiplicar(numero1, numero2);
+ 		break;
+ 	case 4:
+ 		dividir(numero1, numero2);
+ 		break;
+ 	default:
+ 		break;
+ }
+
+
