9e2f69a656ad1edd905fa2a052efe0369aabf1de
alecchamorro <alecchamorro@gmail.com>
2022-08-01 12:24:17 -0300

    1-8

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-const titulos = document.getElementsByClassName("titulos");
-
-titulos[0].innerText = "JS";
-
-ingreso = document.querySelector("#ingreso"),
-btnGuardar = document.querySelector("#btnGuardar")
-mensaje = document.querySelector("#mensaje")
-
-
-btnGuardar.addEventListener("click", () => {
-    mensaje.innerText = ingreso.value;
-    
-})
+let ingreso = prompt(
+    "Ingresa una opción \n 1-Café solo \n 2-Café con leche \n 3-Té \n 4-Té con leche \n 5-Agua caliente \n\nPara salir ingrese SALIR"
+  );
+  if (ingreso == "SALIR") alert("Gracias, que tengas un buen día");
+  
+  while (ingreso != "SALIR") {
+    switch (ingreso) {
+      case "1":
+        alert("Seleccionaste Café, que lo disfrutes");
+        break;
+      case "2":
+        alert("Seleccionaste Café con leche, que lo disfrutes");
+        break;
+      case "3":
+        alert("Seleccionaste Té, que lo disfrutes");
+        break;
+      case "4":
+        alert("Seleccionaste Té con leche, que lo disfrutes");
+        break;
+      case "5":
+        alert("Seleccionaste Agua caliente");
+        break;
+  
+      default:
+        alert("Opción no válida");
+        break;
+    }
+    ingreso = prompt(
+      "Ingresa una opción \n 1-Café solo \n 2-Café con leche \n 3-Té \n 4-Té con leche \n 5-Agua caliente \n\nPara salir ingrese SALIR"
+    );
+    if (ingreso == "SALIR") alert("Gracias, que tengas un buen día");
+  }
