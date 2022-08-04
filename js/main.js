c4a3029c56b851b08551c3c6b4e897fb57db3012
alecchamorro <alecchamorro@gmail.com>
2022-08-04 16:37:08 -0300

    Programación con funciones

--- js/main.js
+++ js/main.js
@@ -1,32 +1,50 @@
-let ingreso = prompt(
-    "Ingresa una opción \n 1-Café solo \n 2-Café con leche \n 3-Té \n 4-Té con leche \n 5-Agua caliente \n\nPara salir ingrese SALIR"
-  );
-  if (ingreso == "SALIR") alert("Gracias, que tengas un buen día");
-  
-  while (ingreso != "SALIR") {
-    switch (ingreso) {
-      case "1":
-        alert("Seleccionaste Café, que lo disfrutes");
-        break;
-      case "2":
-        alert("Seleccionaste Café con leche, que lo disfrutes");
-        break;
-      case "3":
-        alert("Seleccionaste Té, que lo disfrutes");
-        break;
-      case "4":
-        alert("Seleccionaste Té con leche, que lo disfrutes");
-        break;
-      case "5":
-        alert("Seleccionaste Agua caliente");
-        break;
-  
-      default:
-        alert("Opción no válida");
-        break;
-    }
-    ingreso = prompt(
-      "Ingresa una opción \n 1-Café solo \n 2-Café con leche \n 3-Té \n 4-Té con leche \n 5-Agua caliente \n\nPara salir ingrese SALIR"
-    );
-    if (ingreso == "SALIR") alert("Gracias, que tengas un buen día");
-  }
+function saludar() {
+  alert("Calculadora");
+}
+
+saludar();
+
+function sumar(x = 0, y = 0) 
+  return x + y;
+  console.log("Esto no se ejecuta");
+function restar(x, y) {
+  const resultado = x - y;
+  alert(`El resultado de ${x}-${y} es ${resultado}`);
+  return resultado;
+}
+function multiplicar(x, y) {
+  const resultado = x * y;
+  alert(`El resultado de ${x}*${y} es ${resultado}`);
+}
+function dividir(x, y) {
+  const resultado = x / y;
+  alert(`El resultado de ${x}/${y} es ${resultado}`);
+}
+
+const operacion = Number(prompt(`Bienvenideo, que operacion queres realizar?\n
+1 - Sumar \n
+2 - Restar\n
+3 - Multiplicar\n
+4 - Dividir\n`));
+
+const numero1 = Number(prompt("Ingresa el primer número"));
+const numero2 = Number(prompt("Ingresa el segundo número"));
+
+switch (operacion) {
+  case 1:
+    const suma = sumar(numero1, numero2);
+    alert(`El resultado de ${numero1}+${numero2} es ${suma}`);
+    break;
+  case 2:
+    const resta = restar(numero1, numero2);
+    console.log(resta);
+    break;
+  case 3:
+    multiplicar(numero1, numero2);
+    break;
+  case 4:
+    dividir(numero1, numero2);
+    break;
+  default:
+    break;
+}
