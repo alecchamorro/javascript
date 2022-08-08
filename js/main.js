a9fc48048e68dd7e77b803ac116f74f2f7c4ddd8
alecchamorro <alecchamorro@gmail.com>
2022-08-08 17:56:32 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,53 +4,60 @@ function saludar() {
 
 saludar();
 
-function sumar(x, y) {
-  const resultado = x + y;
-  // alert(`El resultado de ${x}+${y} es ${resultado}`); pd Cierto que el return retorna sin necesidad de un alert xd
-  return resultado;
-   }
 
+
+function sumar(x, y) {
+    const resultado = x + y;
+    return alert(`El resultado de ${x}+${y} es ${resultado}`);
+   }
 function restar(x, y) {
-  const resultado = x - y;
-  // alert(`El resultado de ${x}-${y} es ${resultado}`);
-  return resultado;
+    const resultado = x - y;
+    return alert(`El resultado de ${x}-${y} es ${resultado}`);
 }
 function multiplicar(x, y) {
-  const resultado = x * y;
-  // alert(`El resultado de ${x}*${y} es ${resultado}`);
-  return resultado;
+    const resultado = x * y;
+    return alert(`El resultado de ${x}*${y} es ${resultado}`);
 }
 function dividir(x, y) {
-  const resultado = x / y;
-  // alert(`El resultado de ${x}/${y} es ${resultado}`);
-  return resultado
+    const resultado = x / y;
+    return alert(`El resultado de ${x}/${y} es ${resultado}`);
 }
 
-const operacion = Number(prompt(`Bienvenideo, que operacion queres realizar?
-1 - Sumar
-2 - Restar
-3 - Multiplicar
-4 - Dividir`));
 
-const numero1 = Number(prompt("Ingresa el primer número"));
-const numero2 = Number(prompt("Ingresa el segundo número"));
+do{
+const operacion = prompt(`Bienvenido, que operacion queres realizar?\n
+1 - Sumar\n
+2 - Restar\n
+3 - Multiplicar\n
+4 - Dividir\n`);
 
+const numero1 = prompt("Ingresa el primer número");
+const numero2 = prompt("Ingresa el segundo número");
+
+
+if (operacion == "SALIR") {
+    alert("gracias que tenga un buen dia");}
+
+  else{
 switch (operacion) {
-  case 1:
-    const suma = sumar(numero1, numero2);
-    // alert(`El resultado de ${numero1}+${numero2} es ${suma}`);
+  case "1":
+    sumar(numero1, numero2);
     break;
-  case 2:
-    const resta = restar(numero1, numero2);
-  return resta
+  case "2":
+    restar(numero1, numero2);
     break;
-  case 3:
+  case "3":
     multiplicar(numero1, numero2);
     break;
-  case 4:
+  case "4":
     dividir(numero1, numero2);
     break;
+    
   default:
     break;
+    
 }
+}
+}while(operacion != "SALIR");
+
 
