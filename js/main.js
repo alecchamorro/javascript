fdf50328ef4b15feaee83a292be5862334cc59ed
alecchamorro <alecchamorro@gmail.com>
2022-08-29 15:20:02 -0300

    2da_entrega

--- js/main.js
+++ js/main.js
@@ -33,6 +33,20 @@ function addToCarritoItem(e) {
 // pusheo al array de objetos, cada click va cargar el objeto al carrito
 function addItemCarrito(newItem){
 
+  const inputElement = tbody.getElementsByClassName(`input__elemento`)
+  for (let i=0; i< carrito.length; i++){
+    if(carrito[i].tittle.trim()===newItem.tittle.trim()){
+      //trim va quitar todos los espacion que estan a los lados, de esta forma g quedan iguales
+      carrito[i].cantidad++//cada vez que esta condicion se cumpla entonces se va sumar
+      const inputValue = inputElement[i]
+      inputValue.value++
+      carritoTotal()
+
+      return null; //de esta forma no se va ejectutar el push ni el render, simplemente va salir de la funcion
+    }
+
+  }
+
   carrito.push(newItem)
  //cargo todo correctamente los objos al array carrito
  console.log(carrito);//array de objetos por log
@@ -48,14 +62,16 @@ function renderCarrito () { //renderizar los datos en la seccion de carrito, con
   const content = `
   
 
-  <th scope="row">1</th>
+  <th scope="row"></th>
             <td class="table__productos"><img src=${item.img} alt="" width="100px" height="100px">
               <h6 class="title">${item.tittle}</h6></td>
 
             <td class="table__price"><p>${item.precio}</p></td>
             <td class="table__Cantidad">
-             <input type="number" min="1" value=${item.cantidad}>
-             <button class="delete btn btn-danget xred">X</button>
+             <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+          
+             <button class="btn btn-outline-danger delete btn btn-danget xred">x</button>
+            
               </td>
 
   `
@@ -63,7 +79,68 @@ function renderCarrito () { //renderizar los datos en la seccion de carrito, con
 
   tr.innerHTML = content //al tr le agregamos el contenido dinamico de la const content
   tbody.append(tr) //en el cuerpo del html (tbody) agregamos el contenido dinamico creado con tr.innerHTML (todo lo que esta dentro de content)
+
+  tr.querySelector(".delete").addEventListener(`click`, removeItemCarrito)
+  tr.querySelector(".input__elemento").addEventListener(`change`, sumaCantidad)
 })
+carritoTotal()
+}
+// funcion suma total
+function carritoTotal(){
+  let total = 0;
+  const itemCartTotal = document.querySelector(`.itemCardtotal`)
+  carrito.forEach((item)=>{
+    const precio = Number(item.precio.replace("$",``))
+    total = total + precio*item.cantidad
+
+  
+  })
+  itemCartTotal.innerHTML = `total $${total}`
+  addLocalStorage()
+}
+// boton remover item carrito
+function removeItemCarrito(e){
+  const buttonDelete = e.target
+  const tr = buttonDelete.closest(`.itemCarrito`)
+  const tittle = tr.querySelector(`.title`).textContent
+  for(let i = 0; i<carrito.length; i ++){
+    if(carrito[i].tittle.trim() === tittle.trim()){
+    carrito.splice(i, 1)
+  }
+}
+  tr.remove()
+  carritoTotal()
+}
+// sumar items carrito
+function sumaCantidad(e){
+  const sumaInput = e.target
+  const tr = sumaInput.closest(".itemCarrito")
+  const tittle=tr.querySelector(`.title`).textContent
+  carrito.forEach(item =>{
+    if(item.tittle.trim()===tittle){
+     sumaInput.value <1 ? (sumaInput.value = 1): sumaInput.value
+      item.cantidad = sumaInput.value
+      carritoTotal()
+      
+    }
+  })
+  console.log(carrito);
+}
+
+
+// storage
+
+function addLocalStorage(){
+  localStorage.setItem(`carrito`,JSON.stringify(carrito))
+
+}
+
+window.onload = function(){
+  const storage = JSON.parse(localStorage.getItem(`carrito`))
+  if(storage){
+    carrito = storage
+    renderCarrito()
+  }
 }
 
 
